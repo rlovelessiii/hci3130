89bdf1d24356260bd30878d64aa3d80c53d97919
Richard Loveless III <richardlovelessiii@gmail.com>
2019-04-16 19:51:55 -0400

    updated articles and photos page

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-
+  like = [false, false, false, false];
+  dislike = [false, false, false, false];
   constructor() { }
 
   ngOnInit() {
+  }
+  toggleLike(index) {
+    this.like[index] = !this.like[index];
+  }
+  toggleDislike(index) {
+    this.dislike[index] = !this.dislike[index];
   }
 
 }
